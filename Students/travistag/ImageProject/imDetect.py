3eb10a1fb40abf997b16324ab65f6a27e43f2ddd
Dan Whitten <danielwhitten@gmail.com>
2017-02-28 15:12:07 -0600

    added comments, mean and variance are no hard-coded

--- Students/travistag/ImageProject/imDetect.py
+++ Students/travistag/ImageProject/imDetect.py
@@ -5,42 +5,52 @@ import os
 from PIL import Image
 
 
-def normalizeHisto(h):
-	tot = sum(h)
-	ret = []
-	for v in h:
-		ret.append(v/tot)
-	return ret
+def normalizeHisto(hist_list):
+    """
+    Normalize a 1-channel image histogram. The histogram should be passed
+    as a list. 
+    """
+    total = sum(hist_list)
+    return [v*1./total for v in hist_list]
 
 def classifyImage(m0, s0, m1, s1, img):
-	img = img.convert(mode = 'L')
-	val = max(normalizeHisto(list(img.histogram())))
-	l0 = scipy.stats.norm.pdf(val, m0, s0)
-	l1 = scipy.stats.norm.pdf(val, m1, s1)
-	if(l0>l1):
-		return 0
-	else:
-		return 1
+    """
+    Clasify an image.
+    """
+    # convert image to monochrome
+    img = img.convert(mode = 'L')
+    # calculate a luminance histogram 
+    # normalize it so that images of different sizes can be compared
+    # then get the normalized occurence of the most prevalent intensity
+    val = max(normalizeHisto(list(img.histogram())))
+    # calculate the likelihood of this intensity given hypothesis 0
+    l0 = scipy.stats.norm.pdf(val, m0, s0)
+    # calculate the likelihood of this intensity given hypothesis 1
+    l1 = scipy.stats.norm.pdf(val, m1, s1)
+    # assuming equal priors, make a decision
+    if(l0>l1):
+        return 0
+    else:
+        return 1
 
+# approximate Guassian distribution for real images
+rmean = 0.015091
+rstd = 0.006736
 
-dfr = pd.read_csv('real.csv')
-dfs = pd.read_csv('synth.csv')
-rmean = float(dfr.mean())
-smean = float(dfs.mean())
-rstd = float(dfr.std())
-sstd = float(dfs.std())
+# approximate Guassian distribution for synethic images
+smean = 0.153275
+sstd = 0.030564
 
-
+# loop through all images in a directory and classify them
 if(len(sys.argv)<2):
 	print('Need a directory to predict')
 	exit()
 direct = sys.argv[1]
 predictions = []
-
-
 for filename in os.listdir(direct):
 	im = Image.open(str(direct)+str(filename))
 	predictions.append(classifyImage(rmean, smean, rstd, sstd, im))
 
+# save predictions to file
 df = pd.DataFrame(predictions)
 df.to_csv('predictions.csv', index = False)
